bf7263719c59f1255aadf48b64b3d77c9019d8bc
BuildTools <unconfigured@null.spigotmc.org>
2017-05-03 17:13:46 +0200

    indentation

--- Serie 2/Aufgabe2a.rb
+++ Serie 2/Aufgabe2a.rb
@@ -1,11 +1,11 @@
 def concat(a2d) 
-    result = [] 
-    for arr in a2d
-        for i in arr do
-            result.push(i)
-        end
-    end
-    return result
+	result = [] 
+		for arr in a2d
+			for i in arr do
+				result.push(i)
+			end
+		end
+	return result
 end
 
 p(concat([[2,3], [5,6], [9,10]]))
